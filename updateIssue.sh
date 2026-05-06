681fa5724045ccc554762f9f03810c462e0ea26d
Moppe-xd <mikael.lundkvist.skola@gmail.com>
2026-05-06 16:36:36 +0200

    added script for updating issues
    Needed to update some issues on all the repos, so created a simple
    script to update the relevant repos

---
+++ updateIssue.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+REPOS=(
+  # Here goes the repos that you want to edit the issue in.
+  # Have to be written in the format
+  # Owner/reponame
+
+)
+
+ISSUES=(
+  # Here goes the issues you want to change
+  # Have to be written in the format
+  # issue number / newissue
+)
+
+echo "Starting updating issues"
+
+for REPO in "${REPOS[@]}"; do
+  echo "--------------------------------------------"
+  echo "working on $REPO"
+
+  #Clone the repo if the repo could not be found on the computer
+  if [ ! -d "${REPOS##*/}" ]; then
+    gh repo clone "$REPO"
+  else
+    echo "REPO already cloned. No need to clone."
+  fi
+
+  cd "${REPO##*/}" || {
+    echo "Error: could not enter $REPO"
+    continue
+  }
+
+  for ITEM in "${ISSUES[@]}"; do
+    NUMBER="${ITEM%%|*}"
+    FILE="${ITEM##*|}"
+
+    FILEPATH="../issues/$FILE"
+
+    if [ -f "$FILE" ]; then
+      gh issue edit "$NUMBER" --body "$FILE"
+      echo "Updated issue $NUMBER with $FILEPATH"
+    else
+      echo "Error: could not find $FILEPATH"
+    fi
+    sleep 1
+  done
+
+  #Goes back to the root dirctory and removes the git repo
+  cd ..
+  rm -rf "${REPO##*/}"
+done
